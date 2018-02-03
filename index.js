bed736675107ae61dcb7ca231d14af04cf15bcc7
Lucy <marerescue@gmx.com>
2018-02-03 02:51:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function happyHolidays() {
   return "Happy holidays!"
 }// write your code below!
+}
 
 function happyHolidayTo(name) {
   return ('Happy ${xmas}, ${Lu}!')
 }
+}
+
 function happyHolidayTo(holiday, name) returns "Happy ${Independence Day}, ${you}!"
 
 function holidayCountdown(holiday, days) {
