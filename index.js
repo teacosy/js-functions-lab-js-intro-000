713f9db63569b5f9844b3d671612feeef27ec9d0
Lucy <marerescue@gmx.com>
2018-02-03 02:45:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function happyHolidays() {
 function happyHolidayTo(name) {
   return ('Happy ${xmas}, ${Lu}!')
 }
+function happyHolidayTo(holiday, name) returns "Happy ${Independence Day}, ${you}!"
+
+function holidayCountdown(holiday, days) {
+  return "It's ${5} days until ${xmas}!"
+}
